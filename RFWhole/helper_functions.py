399c06f1ee9ff7caeca3b6887c25a66a9e540b70
seher özçelik <seherozcelik@gmail.com>
2025-03-06 16:10:36 +0300

    Add files via upload

---
+++ RFWhole/helper_functions.py
@@ -0,0 +1,136 @@
+import torch
+import os
+import json
+import csv
+
+import numpy as np
+import pydicom
+import cv2
+import skimage.segmentation as sg
+import math as m
+
+from model import UNet 
+
+def getRFdgms(prediction,RF,CP):
+    allIm = np.where(prediction>0,1,0)
+    radialFiltration = RF.fit_transform(allIm[None, :, :])
+    cubicalPersistence = CP.fit_transform(radialFiltration)
+    return cubicalPersistence[0][:,:-1]
+        
+def get_weight(goldImage,h,w,bounding_box_size):
+    size_weight = {}
+    
+    size_weight['back'] = 2*(h+w) - bounding_box_size
+    
+    organ_list = ['heart','eso','spine', 'lungs']
+
+    sumAll = size_weight['back']
+    for organ in organ_list:
+        im = np.where(goldImage==organ_list.index(organ)+1,1,0)
+        if im.sum() > 0:
+            bnd = sg.find_boundaries(im, mode='inner').astype(np.uint8)
+            xy = np.transpose(np.nonzero(bnd))
+            size_weight[organ] = xy.shape[0]
+            sumAll += size_weight[organ]
+    
+    maxAll=0
+    for key in size_weight.keys():
+        size_weight[key] = sumAll/size_weight[key]
+        if maxAll < size_weight[key]:
+            maxAll = size_weight[key]
+            
+    for key in size_weight.keys():
+        size_weight[key] = size_weight[key]/maxAll  
+        
+    for organ in ['back', 'heart','eso','spine','lungs']:
+        if organ not in size_weight.keys(): size_weight[organ] = size_weight['back']        
+            
+    return size_weight
+       
+
+def prepare_gold_and_weight(label_name, sh, eh, sw, ew, bounding_box_size):
+    goldImage = cv2.imread(label_name,cv2.IMREAD_GRAYSCALE)
+    
+    goldImage = goldImage[sh:eh,sw:ew]
+    
+    (h,w) = goldImage.shape
+    
+    goldLabel = np.zeros((5,h,w)).astype(np.uint8)
+    
+    for i in range(h):
+        for j in range(w):
+            goldLabel[goldImage[i,j],i,j]=1
+            
+
+    goldLabel = goldLabel.astype(np.float32)
+    
+    size_weight = get_weight(goldImage, h,w, bounding_box_size)
+
+    return goldLabel, size_weight
+
+def normalizeImage(img):
+
+    normImg = np.zeros(img.shape) 
+    for i in range(img.shape[0]):
+        if img[i, :, :].std() != 0:
+            normImg[i, :, :] = (img[i, :, :] - img[i, :, :].mean()) / (img[i, :, :].std())
+
+    return normImg.astype(np.float32)
+
+def getData(folder, gold_folder, chosen_data_file, cutting_regions_file, bounding_box_file):
+    
+    with open(chosen_data_file) as f:
+        chosen_data = json.load(f) 
+        
+    with open(cutting_regions_file) as f:
+        cutting_regions = json.load(f)     
+        
+    with open(bounding_box_file) as f:
+        bounding_box_regions = json.load(f)                      
+
+    data = []
+    
+    patients = os.listdir(folder)
+    for patient in patients:
+        images = chosen_data[patient]
+        for image in images:
+            [sh, eh, sw, ew] = cutting_regions[image]
+            
+            [bbsh, bbeh, bbsw, bbew] = bounding_box_regions[image]
+            bounding_box_size = 2*((int(bbeh) - int(bbsh)) + (int(bbew) - int(bbsw)))            
+            
+            im = pydicom.dcmread(folder + '/' + patient + '/' + image).pixel_array
+            im = im[sh:eh,sw:ew]
+            im = np.expand_dims(im,0)
+            im = normalizeImage(im)
+
+            label, size_weight = prepare_gold_and_weight(gold_folder + '/' + image.split('.')[0] + '.png', sh, eh, sw, ew, bounding_box_size)
+
+            data.append([im, label, image.split('.')[0], size_weight])
+
+    return data   
+
+def test(in_channel, first_out_channel, model_name, tst_im_name, gold_im_name, sh=128, eh=384, sw=64, ew=448):
+       
+    model = UNet(in_channel,first_out_channel).cuda()
+
+    model.load_state_dict(torch.load(model_name))
+        
+    model.eval()
+    
+    with torch.no_grad():  
+        im_tst = pydicom.dcmread(tst_im_name).pixel_array
+        im_tst = im_tst[sh:eh,sw:ew]
+        im = np.expand_dims(im_tst,0)
+        im = normalizeImage(im)
+        im = np.expand_dims(im,0)
+
+        label = cv2.imread(gold_im_name,cv2.IMREAD_GRAYSCALE)[sh:eh,sw:ew]
+
+        output = model(torch.FloatTensor(im).cuda())
+        
+        pre_prediction = output.detach().cpu().numpy()[0,:,:,:]
+        prediction = np.argmax(pre_prediction, axis=0)
+
+        return im_tst, pre_prediction, prediction, label
+ 
---
+++ RFWhole/loss.py
@@ -0,0 +1,41 @@
+import torch.nn as nn
+import torch
+import numpy as np
+import json
+import helper_functions as hp
+import gudhi.wasserstein
+
+class WeightedCrossEntropyLoss(nn.Module):
+
+    def __init__(self):
+        super(WeightedCrossEntropyLoss, self).__init__()
+
+    def forward(self, pred, gold, name, size_weight, RF,CP,alpha): 
+        gold2dim = np.argmax(gold[0].numpy(), axis=0)
+        gold = torch.FloatTensor(gold[0]).cuda()
+        pred = pred[0]
+        prediction = np.argmax(pred.detach().cpu().numpy(), axis=0) 
+        prediction_FP_FN_mask = np.where(prediction == gold2dim, 0, 1)
+        prediction_FP_FN = prediction * prediction_FP_FN_mask
+
+        with open('../dataset/goldMulty/rf_dgms0/'+name[0]+'.npy', 'rb') as f:
+            dgms0 = np.load(f)
+
+        pred_dgms0 = hp.getRFdgms(prediction,RF,CP)
+        wd = gudhi.wasserstein.wasserstein_distance(dgms0, pred_dgms0, order=1., internal_p=2.)
+        
+        organ_list = ['back','heart','eso','spine', 'lungs']
+        
+        weight = np.zeros((256,384)).astype(np.float32) 
+        weight_FP_FN = np.zeros((256,384)).astype(np.float32)
+        for organ in organ_list:
+            weight = weight + np.where(gold2dim==organ_list.index(organ),size_weight[organ].item(),0)
+            if organ_list.index(organ) != 0:
+                weight_FP_FN = weight_FP_FN + np.where(prediction_FP_FN==organ_list.index(organ),size_weight[organ].item(),0)
+
+        weightAll = np.where(weight_FP_FN>0,(1 + alpha * wd)*weight_FP_FN,weight)
+        weightAll = torch.FloatTensor(weightAll).cuda()  
+        
+        loss = - torch.sum(gold * torch.log(pred + 1e-8),0)
+    
+        return torch.mean(weightAll * loss)
